49d12a6c4be89b431ad790068a586f71750d0e5d
Jonas Fan <jianfan.tw@gmail.com>
2026-04-05 00:35:33 +0800

    leetcode: 0118

---
+++ problem/leetcode/0001-1000/0118-pascals-triangle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    std::vector< std::vector<int> > generate(int numRows) {
+        std::vector< std::vector<int> > out(numRows);
+
+        for (std::size_t row = 0; row < out.size(); row++) {
+            out[row].resize(row + 1);
+
+            for (std::size_t col = 0; col < out[row].size(); col++) {
+                if (row == 0) {
+                    // First row
+                    out[row][col] = 1;
+                } else if (col == 0) {
+                    // Left boundary
+                    out[row][col] = 1;
+                } else if ((col + 1) == out[row].size()) {
+                    // Right boundary
+                    out[row][col] = 1;
+                } else {
+                    // Inner
+                    out[row][col] = out[row - 1][col - 1] + out[row - 1][col];
+                }
+            }
+        }
+
+        return out;
+    }
+};
