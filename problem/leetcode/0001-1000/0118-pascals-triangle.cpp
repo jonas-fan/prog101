a414f6b903cf419f9939767c65e83c59ec322e0e
Jonas Fan <jianfan.tw@gmail.com>
2026-04-05 00:36:57 +0800

    leetcode: 0118 update

--- problem/leetcode/0001-1000/0118-pascals-triangle.cpp
+++ problem/leetcode/0001-1000/0118-pascals-triangle.cpp
@@ -7,10 +7,7 @@ public:
             out[row].resize(row + 1);
 
             for (std::size_t col = 0; col < out[row].size(); col++) {
-                if (row == 0) {
-                    // First row
-                    out[row][col] = 1;
-                } else if (col == 0) {
+                if (col == 0) {
                     // Left boundary
                     out[row][col] = 1;
                 } else if ((col + 1) == out[row].size()) {
