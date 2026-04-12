59595ba5cc4882a55872c0c001ebd182f173c5e5
Jonas Fan <jonasyfan@gmail.com>
2026-04-12 10:13:28 +0800

    leetcode: 0014

--- problem/leetcode/0001-1000/0014-longest-common-prefix.c
+++ problem/leetcode/0001-1000/0014-longest-common-prefix.c
@@ -1,33 +1,12 @@
-#include <stdlib.h>
-#include <string.h>
-
-static int min(int lhs, int rhs) {
-    return (lhs < rhs) ? lhs : rhs;
-}
-
 char * longestCommonPrefix(char ** strs, int strsSize) {
-    int size = strlen(strs[0]);
-
-    for (int i = 1; i < strsSize; i++) {
-        size = min(size, strlen(strs[i]));
-    }
-
-    for (int j = 0; j < size; j++) {
-        const char c = strs[0][j];
-
-        for (int i = 1; i < strsSize; i++) {
-            if (strs[i][j] != c) {
-                size = j;
-                break;
+    for (int i = 0; strs[0][i]; i++) {
+        for (int j = 1; j < strsSize; j++) {
+            if (strs[j][i] != strs[0][i]) {
+                strs[0][i] = 0;
+                return strs[0];
             }
         }
     }
 
-    char * out = (char *)calloc(size + 1, sizeof(char));
-
-    for (int i = 0; i < size; i++) {
-        out[i] = strs[0][i];
-    }
-
-    return out;
+    return strs[0];
 }
